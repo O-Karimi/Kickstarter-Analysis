641b6c2ff4ebbee519629d00a19e1caf2cfcae3b
O-Karimi <omidkarimi54@gmail.com>
2026-09-18 02:09:05 +0200

    chore: Initial feature drops
    Some features that we didn't need got dropped

--- applied_statistics.r
+++ applied_statistics.r
@@ -23,10 +23,80 @@ colnames(all_data)
 
 #str(all_data)
 
+install.packages("dplyr")
+library("dplyr")
+
 glimpse(all_data)
 
-# Some visualizations
+##looking around for missing values
+summary(all_data)
 
-# Some dependency checks and hypothesis checks
+sum(is.na(all_data))
 
-# Calculating impacts on success of a kickstarter
+colSums(is.na(all_data))
+
+table(all_data$country)
+
+table(all_data$state)
+
+#### As I checked manually there are lots of 0 and nulls but in the forms which are not recognizeable to is.na function by default
+
+initial_cleanup <- all_data
+
+glimpse(initial_cleanup)
+
+install.packages("tidyselect")
+library("tidyselect")
+
+initial_cleanup <- initial_cleanup %>%
+  select(-blurb)
+
+dim(initial_cleanup)
+
+unique(initial_cleanup$country)
+unique(initial_cleanup$country_displayable_name)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-country)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-currency_symbol)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-currency_trailing_code)
+
+unique(initial_cleanup$current_currency)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-current_currency)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-name)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-slug)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-source_url)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-static_usd_rate)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-usd_exchange_rate)
+
+unique(initial_cleanup$usd_type)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-usd_type)
+
+initial_cleanup <- initial_cleanup %>%
+  select(-usd_type)
+
+dim(initial_cleanup)
+
+#--------------- Some visualizations
+
+#---------------- Some dependency checks and hypothesis checks
+
+#------------------ Calculating impacts on success of a kickstarter
